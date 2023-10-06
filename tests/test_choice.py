d347378dda74554920bd343b83fedc1b87f0ba0c
Sander Teunissen <s.teunissen@gmail.com>
2023-10-06 17:08:08 +0200

    add choice help

--- tests/test_choice.py
+++ tests/test_choice.py
@@ -8,6 +8,10 @@ class ModelWithChoice(BaseModel):
     choice: Literal["option1", "option2"]
 
 
+class ModelWithOptionalChoice(BaseModel):
+    choice: Literal["option1", "option2"] = "option1"
+
+
 def test_choice():
     model = parse(ModelWithChoice, ["option1"])
 
@@ -17,6 +21,11 @@ def test_choice():
 def test_wrong_choice():
     with pytest.raises(ValidationError):
         parse(ModelWithChoice, ["wrong_options"])
+
+
+def test_optional_choice():
+    model = parse(ModelWithOptionalChoice, ["--choice", "option2"])
+    assert model.choice == "option2"
 
 
 def test_model_help(capture_output):
